b393ef1ed997fb56b6e65a2261dcc971af731b81
vileda <vileda@piratenfraktion-nrw.de>
2013-09-05 03:17:32 +0200

    trying to add keyfiles

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,3 +19,18 @@ angular.module('stpsApp', ['credentialServices'])
         redirectTo: '/'
       });
   });
+
+function filesModelDirective(){
+  return {
+    controller: function($parse, $element, $attrs, $scope){
+      var exp = $parse($attrs.filesModel);
+
+      $element.on('change', function(){
+        exp.assign($scope, this.files);
+        $scope.$apply();
+      });
+    }
+  };
+}
+
+angular.module('stpsApp').directive('filesModel', filesModelDirective);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,4 +4,18 @@ angular.module('stpsApp').
   controller('MainCtrl', function ($scope, Credential) {
     $scope.credentials = Credential.query();
     $scope.cryptokey = true;
+    $scope.$watch('keyfile', function() {
+      if($scope.keyfile) {
+        var f = $scope.keyfile[0];
+        var reader = new FileReader();
+        reader.onload = (function(f, $scope) {
+          return function(e) {
+            $scope.keyfileData = e.target.result;
+            $scope.cryptokey = btoa($scope.keyfileData).trim();
+          };
+        })(f, $scope);
+        reader.readAsBinaryString(f);
+      }
+      else { $scope.cryptokey = false; }
+    });
   });
